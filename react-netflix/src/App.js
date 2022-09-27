d3d20fcf07b9cf700a4ad7b3196b618f667375ac
blockodyssey-uhui <reah@blockodyssey.io>
2022-09-27 11:25:19 +0900

    feat: 영화 row 영역 생성

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,12 +1,20 @@
+import requests from "./api/request";
 import "./App.css";
 import Banner from "./components/Banner";
 import Nav from "./components/Nav";
+import Row from "./components/Row";
 
 function App() {
     return (
-        <div className="App">
+        <div className="app">
             <Nav />
             <Banner />
+
+            <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginal} isLargeRow />
+            <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+            <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+            <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
         </div>
     );
 }
